12b16b501a162648051e2bebea03440b9ff73c9d
YaWa6 <57521797+YaWa6@users.noreply.github.com>
2019-11-12 14:20:57 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+//Auto suchen
+
+function carsearch(){
+  alert("hallo")
+}
+
+
+
+
 
 //Damit Dropdownliste in Textfeld geschrieben wird
 
@@ -33,12 +42,7 @@ function cardetail(){
 
 
 
-//Bei Klick den Preis ausgeben
-function setslider(thisValue, sliderValue){
-    sliderValue.value=thisValue;
-    
-    document.querySelector(".range span").innerHTML=thisValue;
-}
+
 
 
 // Bei slider verschieben Preis ausgeben
@@ -53,8 +57,97 @@ function setslider(thisValue, sliderValue){
         var thisValue=slider.value;
   
       // Wert des Range-Sliders anzeigen
-      document.querySelector(".range span").innerHTML=thisValue;
+      document.querySelectorAll(".range span")[0].innerHTML=thisValue;
     })
+
+
+
+
+
+    // Bei slider verschieben Leistung ausgeben
+  // Range-Slider in Variable speichern 
+  var slider2 = document.getElementById("powerfield");
+
+  var thisValue2=slider2.value;
+
+  // EventListener für das Verändern des Sliders hinzufügen
+  slider2.addEventListener("change", function(){
+
+      var thisValue2=slider2.value;
+
+    // Wert des Range-Sliders anzeigen
+    document.querySelectorAll(".range span")[1].innerHTML=thisValue2;
+    
+  })
+
+
+
+
+
+
+   // Bei slider verschieben km ausgeben
+  // Range-Slider in Variable speichern 
+  var slider3 = document.getElementById("km");
+
+  var thisValue3=slider3.value;
+
+  // EventListener für das Verändern des Sliders hinzufügen
+  slider3.addEventListener("change", function(){
+
+      var thisValue3=slider3.value;
+
+    // Wert des Range-Sliders anzeigen
+    document.querySelectorAll(".range span")[2].innerHTML=thisValue3;
+  })
+
+
+
+
+//Bei Klick richtige Werte angeben
+//Bei Klick den Preis ausgeben
+function setslider(thisValue, sliderValue){
+  sliderValue.value=thisValue;
+  
+  document.querySelector(".range span").innerHTML=thisValue;
+}
+
+
+//Bei Klick die Leistung ausgeben
+function setslider2(thisValue2, sliderValue){
+  sliderValue.value=thisValue2;
+  
+  document.querySelectorAll(".range span")[1].innerHTML=thisValue2;
+
+}
+
+
+
+//Bei Klick km ausgeben
+function setslider3(thisValue3, sliderValue){
+  sliderValue.value=thisValue3;
+  
+  document.querySelectorAll(".range span")[2].innerHTML=thisValue3;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -67,14 +160,22 @@ function resetSelection(){
     var pricefield = document.getElementById('pricefield');
     var powerfield = document.getElementById('powerfield');
     var km = document.getElementById('km');
+
+
   
+    
+    
 
     pollutionClass.value = '';
     Markenfeld.value = '';
     modelfield.value = '';
-    pricefield.value = '';
-    powerfield.value = '';
-    km.value = '';
+    pricefield.value = '15000';
+    powerfield.value = '117';
+    km.value = '30000';
+    
+    document.querySelectorAll(".range span")[0].innerHTML= '15000';
+    document.querySelectorAll(".range span")[1].innerHTML= '117';
+    document.querySelectorAll(".range span")[2].innerHTML= '30000';
 }
 
 
@@ -85,9 +186,12 @@ function resetDetailSelection(){
     var consumptionOut = document.getElementById('consumptionOut');
     var consumptionIn = document.getElementById('consumptionIn');
     var fuel = document.getElementById('fuel');
-    var nationalEmissionclass = document.getElementById('nationalEmissionclass');
-    var ManufacturerSign = document.getElementById('ManufacturerSign');
     var carType = document.getElementById('carType');
+    var TypSign = document.getElementById('TypSign');
+    var ManufacturerSign = document.getElementById('ManufacturerSign');
+    var carClass = document.getElementById('carClass');
+
+
 
   
 
@@ -96,9 +200,11 @@ function resetDetailSelection(){
     consumptionOut.value = '';
     consumptionIn.value = '';
     fuel.value = '';
-    nationalEmissionclass.value = '';
-    ManufacturerSign.value = '';
     carType.value = '';
+    TypSign.value = '';
+    ManufacturerSign.value = '';
+    carClass.value = '';
+
 
 }
     
